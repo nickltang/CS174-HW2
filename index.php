3a8f5ed4f3a2d87721ed6ce2ddd03f9472812da3
Nick <nicholasltang21@gmail.com>
2022-10-05 22:19:13 -0700

    Finished differentiating add/edit page

--- index.php
+++ index.php
@@ -15,19 +15,6 @@ $activity();
  */
 function landingController()
 {
-    // Dummy data
-    // $data["PIZZA_ENTRIES"] = [
-    //     'Fromage Delight' => [
-    //         'price' => 12,
-    //         'visits' => 30,
-    //         'toppings' => ['cheese', 'pepperoni']
-    //     ],
-    //     'Peppy Pizzazz' => [
-    //         'price' => 30,
-    //         'visits' => 5,
-    //         'toppings' => []
-    //     ]
-    // ];C
     // Gets pizza entries from pizza.txt file
     $data["PIZZA_ENTRIES"] = getPizzaEntries();
     $data["PIZZA_ENTRIES"] = checkForPizzaUpdates($data["PIZZA_ENTRIES"]);   
@@ -68,7 +55,6 @@ function confirmController()
         ? filter_var($_REQUEST['name'], FILTER_SANITIZE_SPECIAL_CHARS) 
         : "";
     $data["ENTRIES"] = getPizzaEntries();
-    // $data["ENTRIES"] = deletePizzaEntry($data);
     
     $layout = (isset($_REQUEST['f']) && in_array($_REQUEST['f'], ["html"])) 
         ? $_REQUEST['f'] . "Layout" 
@@ -85,6 +71,10 @@ function editController()
         ? filter_var($_REQUEST['name'], FILTER_SANITIZE_SPECIAL_CHARS) 
         : "";
     $data["ENTRIES"] = getPizzaEntries(); 
+
+    if(array_key_exists($data["NAME"], $data["ENTRIES"])) {
+        $data["currentPizza"] = $data["ENTRIES"][$data["NAME"]];
+    }
 
     $layout = (isset($_REQUEST['f']) && in_array($_REQUEST['f'], ["html"])) 
         ? $_REQUEST['f'] . "Layout" 
@@ -166,22 +156,9 @@ function menuView($data) {
         }
         ?>
         <div class="text-center">
-            <?php
-                if(!empty($data["PIZZA_ENTRIES"])) {
-            ?>
-                <a href=index.php?a=edit&name=<?=urlencode($name)?>>
-                    <button class="addPieBtn">Add Pie</button></th>
-                </a>
-            <?php
-                }
-                else {
-            ?>
-                <a href=index.php?a=edit>
-                    <button class="addPieBtn">Add Pie</button></th>
-                </a>  
-            <?php
-                }
-            ?>      
+            <a href=index.php?a=edit>
+                <button class="addPieBtn">Add Pie</button></th>
+            </a>      
         </div>    
     </div>
     
@@ -190,9 +167,9 @@ function menuView($data) {
 
 
 function editView($data) {
-    var_dump($data);
+    // Header for add pizza
     $header = "Add a Pie";
-    if($data["NAME"] != "") {
+    if(isset($data["currentPizza"])) {
         $header = "Pie Editor";
     }
     ?>
@@ -202,8 +179,33 @@ function editView($data) {
         </a>
         <h2><?=$header?></h2>
         <form action="index.php" method="post">
-            <input type="text" placeholder="Pie Name" name="name">
-            <input type="text" placeholder="Price" name="price">
+            <label for="name">Enter Pizza Name:</label>
+            <?php
+            if(isset($data["currentPizza"])) {
+            ?>
+                <input type="text" placeholder="Pie Name" name="name" value=<?=$data["NAME"]?>>
+            <?php
+            } else {
+            ?>
+                <input type="text" placeholder="Pie Name" name="name">
+            <?php
+            }
+            ?>
+
+            <br/>
+            <br/>
+            <label for="price">Enter Pizza Price ($):</label>
+            <?php
+            if(isset($data["currentPizza"])) {
+            ?>
+                <input type="text" placeholder="Price" name="price" value=<?=(int) $data["currentPizza"]["price"]?>>
+            <?php
+            } else {
+            ?>
+                <input type="text" placeholder="Price" name="price">
+            <?php
+            }
+            ?>
             <div class="table-center">
                 <table id="Pie Editor">
                     <tr>
@@ -211,41 +213,121 @@ function editView($data) {
                     </tr>
                     <tr>
                         <td> 
-                            <input type="checkbox" id="red-sauce" name="toppings[red-sauce]">
+                            <?php
+                            if(isset($data["currentPizza"]) && in_array("red-sauce", $data["currentPizza"]["toppings"])) {
+                            ?> 
+                                <input type="checkbox" id="red-sauce" name="toppings[red-sauce]" checked>
+                            <?php
+                            } else {
+                            ?>
+                                <input type="checkbox" id="red-sauce" name="toppings[red-sauce]">
+                            <?php
+                            }
+                            ?>
                             <label for="red-sauce">Red Sauce</label>
                         </td>
                         <td>
-                            <input type="checkbox" id="green-peppers" name="toppings[green-peppers]">
+                            <?php
+                            if(isset($data["currentPizza"]) && in_array("green-peppers", $data["currentPizza"]["toppings"])) {
+                            ?> 
+                                <input type="checkbox" id="green-peppers" name="toppings[green-peppers]" checked>
+                            <?php
+                            } else {
+                            ?> 
+                                <input type="checkbox" id="green-peppers" name="toppings[green-peppers]">
+                            <?php
+                            }
+                            ?>
                             <label for="green-peppers">Green Peppers</label>
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <input type="checkbox" id="mozarella" name="toppings[mozarella]">
+                            <?php
+                            if(isset($data["currentPizza"]) && in_array("mozarella", $data["currentPizza"]["toppings"])) {
+                            ?> 
+                                <input type="checkbox" id="mozarella" name="toppings[mozarella]" checked>
+                            <?php
+                            } else {
+                            ?> 
+                                <input type="checkbox" id="mozarella" name="toppings[mozarella]">
+                            <?php
+                            }
+                            ?>
                             <label for="mozarella">Mozarella</label>
                         </td>
                         <td>
-                            <input type="checkbox" id="ham" name="toppings[ham]">
+                            <?php
+                            if(isset($data["currentPizza"]) && in_array("ham", $data["currentPizza"]["toppings"])) {
+                            ?> 
+                                <input type="checkbox" id="ham" name="toppings[ham]" checked>
+                            <?php
+                            } else {
+                            ?> 
+                                <input type="checkbox" id="ham" name="toppings[ham]">
+                            <?php
+                            }
+                            ?>
                             <label for="ham">Ham</label>
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <input type="checkbox" id="pepperoni" name="toppings[pepperoni]">
+                            <?php
+                            if(isset($data["currentPizza"]) && in_array("pepperoni", $data["currentPizza"]["toppings"])) {
+                            ?> 
+                                <input type="checkbox" id="pepperoni" name="toppings[pepperoni]" checked>
+                            <?php
+                            } else {
+                            ?> 
+                                <input type="checkbox" id="pepperoni" name="toppings[pepperoni]">
+                            <?php
+                            }
+                            ?>
                             <label for="pepperoni">Pepperoni</label>
                         </td>
                         <td>
-                            <input type="checkbox" id="mushrooms" name="toppings[mushrooms]">
+                            <?php
+                            if(isset($data["currentPizza"]) && in_array("mushrooms", $data["currentPizza"]["toppings"])) {
+                            ?> 
+                                <input type="checkbox" id="mushrooms" name="toppings[mushrooms]" checked>
+                            <?php
+                            } else {
+                            ?> 
+                                 <input type="checkbox" id="mushrooms" name="toppings[mushrooms]">
+                            <?php
+                            }
+                            ?>
                             <label for="mushrooms">Mushrooms</label>
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <input type="checkbox" id="pineapple" name="toppings[pineapple]">
+                            <?php
+                            if(isset($data["currentPizza"]) && in_array("pineapple", $data["currentPizza"]["toppings"])) {
+                            ?> 
+                                <input type="checkbox" id="pineapple" name="toppings[pineapple]" checked>
+                            <?php
+                            } else {
+                            ?> 
+                                <input type="checkbox" id="pineapple" name="toppings[pineapple]">
+                            <?php
+                            }
+                            ?>
                             <label for="pineapple">Pineapple</label>
                         </td>
                         <td>
-                            <input type="checkbox" id="anchovies" name="toppings[anchovies]">
+                            <?php
+                            if(isset($data["currentPizza"]) && in_array("anchovies", $data["currentPizza"]["toppings"])) {
+                            ?> 
+                                <input type="checkbox" id="anchovies" name="toppings[anchovies]" checked>
+                            <?php
+                            } else {
+                            ?> 
+                                <input type="checkbox" id="anchovies" name="toppings[anchovies]">
+                            <?php
+                            }
+                            ?>
                             <label for="anchovies">Anchovies</label>
                         </td>
                     </tr>
@@ -317,7 +399,6 @@ function confirmView($data) {
                 <button type="button">Cancel</button>
             </a>
         </form>
-        
     </div>
     <?php
 }
@@ -345,10 +426,7 @@ function checkForPizzaUpdates($entries) {
     // Add/Edit Pizza
     if(array_key_exists("add", $_POST)) {
         $visits = 0;
-
         // If pizza views in entries, keep existing views value
-        if(array_key_exists($_POST["name"], $entries)) 
-            $visits = (int) $entries[$_POST["name"]["views"]];
 
         // Format toppings into array
         $addToppings = [];
@@ -357,12 +435,17 @@ function checkForPizzaUpdates($entries) {
                 array_push($addToppings, $topping);
         }
 
-        // Update entries array
-        $entries[$_POST["name"]] = [
-            "price" => $_POST["price"],
-            "visits" => $visits,
-            "toppings" => $addToppings
-        ];
+        // Add/update pizza in entries array
+        if(array_key_exists($_POST["name"], $entries)) {
+            $entries[$_POST["name"]]["price"] = $_POST["price"];
+            $entries[$_POST["name"]]["toppings"] = $addToppings;
+        } else {
+            $entries[$_POST["name"]] = [
+                "price" => $_POST["price"],
+                "visits" => 0,
+                "toppings" => $addToppings
+            ];    
+        }        
     }
 
     // Delete Pizza
